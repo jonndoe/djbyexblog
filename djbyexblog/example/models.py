17015c0476f40eb664ce3043aed6226a784eed8f
jonndoe <qb89@yandex.ru>
2020-02-25 23:33:56 +0300

    Implement cheese country_of_origin

--- djbyexblog/example/models.py
+++ djbyexblog/example/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from autoslug import AutoSlugField
 from model_utils.models import TimeStampedModel
 
+from django_countries.fields import CountryField
+
 # TimeStampedModel to auto add created / modified fields
 class Cheese(TimeStampedModel):
 
@@ -24,6 +26,7 @@ class Cheese(TimeStampedModel):
                                 max_length=20,
                                 choices=Firmness.choices,
                                 default=Firmness.UNSPECIFIED)
+    country_of_origin = CountryField("Country of Origin", blank=True)
 
     def __str__(self):
         return self.name
