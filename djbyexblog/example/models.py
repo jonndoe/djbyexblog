61af7601767a0645be9c538a7957f8fd9623c797
jonndoe <qb89@yandex.ru>
2020-02-26 00:16:30 +0300

    Add creator field to Cheese model

--- djbyexblog/example/models.py
+++ djbyexblog/example/models.py
@@ -7,6 +7,8 @@ from django_countries.fields import CountryField
 
 from django.urls import reverse
 
+from django.conf import settings
+
 # TimeStampedModel to auto add created / modified fields
 class Cheese(TimeStampedModel):
 
@@ -29,6 +31,11 @@ class Cheese(TimeStampedModel):
                                 choices=Firmness.choices,
                                 default=Firmness.UNSPECIFIED)
     country_of_origin = CountryField("Country of Origin", blank=True)
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
 
     def __str__(self):
         return self.name
