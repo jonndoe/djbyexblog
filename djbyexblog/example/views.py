b36a5afc1e05941f8d04692c2f124de57bf10254
jonndoe <qb89@yandex.ru>
2020-02-26 00:09:58 +0300

    added LoginRequiredMixin

--- djbyexblog/example/views.py
+++ djbyexblog/example/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import (ListView,
                                   DetailView,
                                   CreateView)
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
 from .models import Cheese
 
 
@@ -13,7 +16,7 @@ class CheeseDetailView(DetailView):
     model = Cheese
 
 
-class CheeseCreateView(CreateView):
+class CheeseCreateView(LoginRequiredMixin, CreateView):
     model = Cheese
     fields = ['name', 'description', 'firmness', 'country_of_origin']
 
